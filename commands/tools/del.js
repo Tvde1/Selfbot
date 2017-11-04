47ff2255ee859399239dca468c7c215a99490d2d
Tim van den Essen <tim3345@live.nl>
2017-11-04 23:26:23 +0100

    better explanation

--- commands/tools/del.js
+++ commands/tools/del.js
@@ -8,7 +8,7 @@ class DelCommand extends Command {
     }
 
     async run(message, args) {
-        if (isNaN(args[0]) || args[0] === ' ') throw new Error(`The correct syntax is ${this.client.config.prefix}${this.info.usage}`);
+        if (isNaN(args[0]) || args[0] === ' ') throw new Error(`The correct syntax is \`${this.client.config.prefix}${this.info.usage}\`.`);
         const number = parseInt(args[0]);
 
         message.Success();
