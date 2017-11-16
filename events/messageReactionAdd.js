2dbf6dfc40839f1f7fa56d1375bb60502b1acd25
Tim van den Essen <tim3345@live.nl>
2017-11-16 08:30:48 +0100

    move these to the config
    don't even dare to find these channels

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -9,15 +9,7 @@ module.exports = async (client, reaction, user) => {
         let options = image ? { files: [ image.url ]} : null;
         let message = image ? null : `Lol! ${reaction.message.author.username} said:\n"${reaction.message.content}"`;
 
-        const memeChannels = [
-            '210041381874302976',
-            '236881526594207744',
-            '272118370206351360',
-            '290592579844833281',
-            '357431715427516418'
-        ];
-
-        for (const channel of memeChannels) {
+        for (const channel of client.config.memechannels) {
             if (channel === reaction.message.channel.id) continue;
             client.channels.get(channel).send(message, options).catch(console.err);
         }
