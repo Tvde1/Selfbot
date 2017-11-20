7c13fdb3d84e46569fe92b56348e2f44553aa8ad
Tim van den Essen <tim3345@live.nl>
2017-11-20 11:16:27 +0100

    nicer stats command

--- commands/tools/stats.js
+++ commands/tools/stats.js
@@ -1,9 +1,11 @@
-const Discord = require('discord.js');
-const moment = require('moment');
+const { type, release, uptime } = require('os');
+const { cpuLoad, memoryUsage }  = require('os-toolbox');
+const { execSync }              = require('child_process');
+const CommandInfo               = require('../../templates/commandInfo');
+const Command                   = require('../../templates/command');
+const Discord                   = require('discord.js');
+const moment                    = require('moment');
 require('moment-duration-format');
-
-const CommandInfo = require('../../templates/commandInfo');
-const Command     = require('../../templates/command');
 
 module.exports = class extends Command {
 
@@ -13,13 +15,39 @@ module.exports = class extends Command {
 
     async run(message) {
         const duration = moment.duration(this.client.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
+        const xd = uptime();
+        const uptimeNumber = moment.duration(xd * 1000).format(' D [days], H [hrs], m [mins], s [secs]');
+        const usedMemory = await memoryUsage();
+        const maxMemory = process.env.LOCAL ? 8096 : 512;
+
+        await message.edit('Loading...');
+
         message.edit(`${message.content}\n\`\`\`asciidoc\n= STATISTICS =
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${this.client.users.size.toLocaleString()}
-• Guilds     :: ${this.client.guilds.size.toLocaleString()}
-• Channels   :: ${this.client.channels.size.toLocaleString()}
-• Discord.js :: v${Discord.version}
-• Node       :: ${process.version}\`\`\``);
+Bot:
+• Uptime           :: ${duration}
+• Users            :: ${this.client.users.size.toLocaleString()}
+• Heartbeat Ping   :: ${Math.round(this.client.ping)}ms
+• Message Ping     :: ${Math.round(message.editedTimestamp - message.createdTimestamp)}ms
+• Bot RAM Usage    :: ${Math.round((process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100)} MB
+
+Guilds and Channels:
+• Guilds           :: ${this.client.guilds.size}
+• Members          :: ${this.client.guilds.map(x => x.members.size).reduce((count, guild) => count += guild)}
+• Emojis           :: ${this.client.emojis.size}
+• Text Channels    :: ${this.client.channels.filter(channel => channel.type === 'text').size}
+• Voice Channels   :: ${this.client.channels.filter(channel => channel.type === 'voice').size}
+• Categories       :: ${this.client.channels.filter(channel => channel.type === 'category').size}
+
+System:
+• OS Type          :: ${String(type).replace('_', '-')} v${release}
+• System Uptime    :: ${uptimeNumber}
+• Sytem RAM Usage  :: ${usedMemory}% (${Math.round((usedMemory / 100) * maxMemory)} MB / ${process.env.LOCAL ? '8 GB' : '512 MB'})
+• System CPU Usage :: ${await cpuLoad()}%
+
+Versions:
+• Discord.js       :: v${Discord.version}
+• Node             :: ${process.version}
+• NPM              :: ${String(execSync('npm -v'))}
+\`\`\``);
     }
 };
